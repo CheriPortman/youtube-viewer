4ec28af869b7d53b6350cabe77f191cadc0a300e
Cheri Portman <cheriportman@hotmail.com>
2019-07-11 09:30:09 -0700

    VideoCard has additional fields

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -5,10 +5,12 @@ const VideoCard = ({ video }) => {
 
   return (
     <div>
+      <div>{video.snippet.channelTitle}</div>
       <img 
         src={video.snippet.thumbnails.medium.url} 
         alt={video.snippet.channelTitle} 
       />
+      <div>{video.snippet.description}</div>
     </div>
   )
 }

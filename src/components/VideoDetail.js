30afce6727da7190ea624c11e0c92d7441f3d401
Cheri Portman <cheriportman@hotmail.com>
2019-07-11 10:31:24 -0700

    iframe constructed, conditional for if video is not selected

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const VideoDetail = ({ video }) => {
+  if (!video) {
+    return <div>Search a video, see a video...</div>
+  }
+  const videoUrl = `https://www.youtube.com/embed/${video.id.videoId}`;
 
   return (
-    <div>{video}</div>
+    <>
+      <div>video goes here{video}</div>
+      <iframe src={videoUrl} title={video.thumbnails.title} />
+    </>
   )
 }
 
